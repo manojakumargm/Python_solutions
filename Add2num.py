113473647ccce5962b177a2b3e7649437a9eea61
ManojKumar G M <manojkumargmgvp@gmail.com>
2024-05-17 12:24:02 +0530

    Add files via upload

---
+++ Add2num.py
@@ -0,0 +1,9 @@
+# Adding two Numbers
+
+# Take The Input From The user
+num_1 = int(input("enter any number:"))
+num_2 = int(input("enter any number:"))
+
+# printing the output
+
+print("the Sum of two number is",num_1+num_2)
